2e40456f668dfada6ab51db6c5511a7f9fd2b645
Alexdelia <alexandre.delille.57@gmail.com>
2023-03-09 18:33:34 +0100

    making an argument parser library

---
+++ lib/sap/sap_derive/src/lib.rs
@@ -0,0 +1,5 @@
+use proc_macro::TokenStream;
+use quote::quote;
+// use syn::parse::{Parse, Parser};
+
+#[proc_macro_derive(Arg)]
---
+++ lib/sap/src/lib.rs
@@ -0,0 +1,6 @@
+struct St {
+    name: String,
+    number: u8,
+}
+
+pub fn usage<S>(st: S) {}
