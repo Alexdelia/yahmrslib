33c66d805e0979db361f1bf8cfd790e9c2c1bfda
Alexdelia <alexandre.delille.57@gmail.com>
2023-03-10 11:13:59 +0100

    just gonna use clap for now

--- lib/sap/sap_derive/src/lib.rs
+++
@@ -1,5 +0,0 @@
-use proc_macro::TokenStream;
-use quote::quote;
-// use syn::parse::{Parse, Parser};
-
-#[proc_macro_derive(Arg)]
--- lib/sap/src/lib.rs
+++
@@ -1,6 +0,0 @@
-struct St {
-    name: String,
-    number: u8,
-}
-
-pub fn usage<S>(st: S) {}
